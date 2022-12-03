9493e59034ffe1a293b3a94510b02effcd36fd88
Yonatan Leulseged <yleulseged@gmail.com>
2022-12-02 19:41:59 -0800

    Create deliverable3.php

---
+++ deliverable3.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form action="welcome.php" method="post">
+Username: <input type="text" name="username"><br>
+Legend name: <input type="text" name="legendname"><br>
+<input type="submit">
+</form
+>
+
+<?php
+echo("<br>"."Top"."<br>");
+$servername = "ecs-pd-proj-db.ecs.csus.edu";
+$username = "CSC174030";
+$password = "Csc134_409292268";
+$mydb = "CSC174030";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$mydb);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+//INSERT
+
+/*
+$sqlPlayerInsert = "INSERT INTO PLAYER (username, legendName)
+VALUES ('GARY', 'Second')";
+
+$sqlLegendInsert = "INSERT INTO LEGEND (legendName,legendType,daggerName,shieldStrength,horseName)
+VALUES ('Second','Assassin','knife',NULL,NULL)";
+
+
+if ($conn->query($sqlLegendInsert) === TRUE) {
+  echo "New Player created successfully";
+} else {
+  echo "Error: " . $sqlLegendInsert . "<br>" . $conn->error;
+  }
+
+if ($conn->query($sqlPlayerInsert) === TRUE) {
+  echo "New Legend record created successfully";
+} else {
+  echo "Error: " . $sqlPlayerInsert . "<br>" . $conn->error;
+}
+*/
+
+//SELECT
+$sql = "SELECT username, legendName FROM PLAYER";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br>"."<br>"."Username: ".$row["username"]."<br>"." Legend name: ".$row["legendName"]."<br>";
+    }
+} else {
+echo "0 results";
+
+}
+
+
+echo("<br>"."Bottom"."<br>");
+mysqli_close($conn);
+?>
+
+</body>
+</html>
